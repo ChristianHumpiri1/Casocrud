0fcb020b238080d0c243ae597ce4649cca62248f
ChristianHumpiri <christianquispe@upeu.edu.pe>
2020-07-17 14:15:09 -0500

    crud

---
+++ src/main/java/pe/edu/upeu/casocrud/CasocrudApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.casocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasocrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/casocrud/CasocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.casocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
